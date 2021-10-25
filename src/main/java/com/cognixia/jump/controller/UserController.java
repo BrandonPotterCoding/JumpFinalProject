bd997238dc0e2a464391a8632ec916821f6a9f5b
mihirbpatel1 <mihirbpatel96@gmail.com>
2021-10-25 15:37:13 -0600

    frontend changes

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,7 +32,7 @@ import com.cognixia.jump.service.MyUserDetailsService;
 import com.cognixia.jump.service.UserService;
 import com.cognixia.jump.util.JwtUtil;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class UserController {
